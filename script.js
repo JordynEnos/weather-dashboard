0d4fe16290e96bf98099b3d96e48880517a94ac0
JordynEnos <jordynenos11@gmail.com>
2023-03-27 16:09:58 -0700

    Added var & API key to javascript.

---
+++ script.js
@@ -0,0 +1,10 @@
+var cityInputEl = document.getElementById("city-input");
+var cityFormEl = document.getElementById("city-form");
+var searchEl = document.getElementById("search-button");
+var searchHistoryEl = document.getElementById("search-history")
+var currentContainerEl = document.getElementById("current-container")
+var forecastContainerEl = document.getElementById("forecast-container")
+
+var APIkey = "1f40db029d8aa5986ddf3ab9927c8d74";
+var cities = []
+
