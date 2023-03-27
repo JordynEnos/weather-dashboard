22f8f91bf43a63df45213254551b9bd12ea9cf69
JordynEnos <jordynenos11@gmail.com>
2023-03-27 16:12:13 -0700

    Input 'loadCities' & 'saveCities' functions.

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ var forecastContainerEl = document.getElementById("forecast-container")
 var APIkey = "1f40db029d8aa5986ddf3ab9927c8d74";
 var cities = []
 
+var loadCities = function() {
+    var citiesLoaded = localStorage.getItem("cities")
+    if(!citiesLoaded) {
+        return false;
+    }
+    
+    citiesLoaded = JSON.parse(citiesLoaded);
+    
+    for (var i=0; i < citiesLoaded.length; i++) {
+        displaySearchedCities(citiesLoaded[i])
+        cities.push(citiesLoaded[i])
+    }
+}
+
+var saveCities = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
